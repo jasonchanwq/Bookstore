a338720e01c952852c319fc6d92d176f3633884f
jasonchanwq <jasonchanwq@gmail.com>
2024-04-20 15:44:07 +0800

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('express-async-errors');
-
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
+app.use(cors());
 require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
